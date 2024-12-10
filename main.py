2210ba8acfbc6a2dbab35f8daf15c4daed045091
M1K45 <272570@student.pwr.edu.pl>
2024-12-10 18:53:42 +0100

    reprezentation data as pytorch tensor

--- main.py
+++ main.py
@@ -6,5 +6,15 @@ from torchvision.transforms import ToTensor
 import csv
 import numpy as np 
 
-data_numpy = np.loadtxt("./data.csv", dtype=int, delimiter=",", skiprows=1)
-print(data_numpy)
+#loading data to numpy matrix 
+data_path = "./data.csv"
+data_numpy = np.loadtxt(data_path, dtype=int, delimiter=",", skiprows=1)
+# print(data_numpy)
+
+col_names = next(csv.reader(open(data_path), delimiter=','))
+print (col_names)
+
+#Numpy to pytorch tensor 
+data_pytorch = torch.from_numpy(data_numpy)
+print(data_pytorch)
+print(data_pytorch.shape)
